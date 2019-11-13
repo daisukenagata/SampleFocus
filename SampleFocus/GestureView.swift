2a6aaad07934f7103e42f4378db05c94d0d8b7de
永田大祐 <dbank0208@gmail.com>
2019-11-13 14:23:21 +0900

    add GestureView

---
+++ SampleFocus/GestureView.swift
@@ -0,0 +1,110 @@
+//
+//  GestureView.swift
+//  SampleFocus
+//
+//  Created by 永田大祐 on 2019/11/13.
+//  Copyright © 2019 永田大祐. All rights reserved.
+//
+
+import UIKit
+
+struct CommonStructure {
+    // ジャスチャー
+    static var swipePanGesture = UIPanGestureRecognizer()
+}
+
+
+final class GestureView: UIView, UIGestureRecognizerDelegate {
+
+    var gestureObject = GestureObject()
+
+    init(_ frame: CGRect? = nil, imageSt: String? = nil) {
+        super.init(frame: frame ?? CGRect())
+
+        initSetting(imageSt: imageSt ?? "")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func initSetting(imageSt: String) {
+        // ジャスチャー
+        CommonStructure.swipePanGesture = UIPanGestureRecognizer(target: self, action:#selector(panTapped))
+        CommonStructure.swipePanGesture.delegate = self
+
+        self.addGestureRecognizer( CommonStructure.swipePanGesture)
+        self.backgroundColor = .blue
+
+        gestureObject.imageView?.image = UIImage(named: imageSt)
+        gestureObject.imageView?.contentMode = .scaleAspectFit
+        gestureObject.imageView?.frame = CGRect(x: 30, y: 30, width: self.frame.width - 60, height: self.frame.height - 30)
+        self.addSubview(gestureObject.imageView ?? UIImageView())
+        // レイヤーのマスキング
+        gestureObject.cALayerView?.tori(gestureObject, bool: true)
+
+        self.layer.addSublayer(gestureObject.cALayerView?.hollowTargetLayer ?? CALayer())
+        self.addSubview(gestureObject.cALayerView ?? UIView())
+
+        self.addSubview(gestureObject.lineDashView ?? UIView())
+        gestureObject.lineDashView?.isHidden = true
+    }
+    
+    @objc func panTapped(sender:UIPanGestureRecognizer) {
+        let position: CGPoint = sender.location(in: self)
+        //画面をなぞる場合にフォーカスの設定
+        self.gestureObject.cALayerView?.effect(gestureObject, bool: false)
+        self.gestureObject.cALayerView?.tori(gestureObject, bool: false)
+        switch sender.state {
+        case .ended:
+            //指が離れた際の座標を取得
+            gestureObject.endPoint = gestureObject.lineDashView?.frame.origin ?? CGPoint()
+            gestureObject.endFrame = gestureObject.lineDashView?.frame ?? CGRect()
+            //フォーカス計算のメソッド呼び出し
+            gestureObject.timerFlag = false
+            timerSetting()
+            break
+        case .possible:
+            break
+        case .began:
+            gestureObject.lineDashView?.isHidden = false
+            gestureObject.timerFlag = true
+            gestureObject.timer.invalidate()
+            //タップした領域を取得
+            gestureObject.touchFlag = self.gestureObject.cropEdgeForPoint(point: gestureObject.framePoint)
+            break
+        case .changed:
+             //タップされた領域からMaskするViewのサイズ、座標計算
+            self.gestureObject.updatePoint(point: position,touchFlag: self.gestureObject.touchFlag)
+            break
+        case .cancelled:
+            break
+        case .failed:
+            break
+        @unknown default:
+            fatalError()
+        }
+    }
+
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        let position: CGPoint = touch.location(in: self)
+        gestureObject.framePoint = position
+        return true
+    }
+
+    func timerSetting() {
+        gestureObject.timer = Timer.scheduledTimer(timeInterval:3.0,
+                                     target: self,
+                                     selector: #selector(animetionSet),
+                                     userInfo: nil,
+                                     repeats: false)
+    }
+
+    @objc func animetionSet() {
+        if gestureObject.timerFlag == false {
+            gestureObject.matchGround()
+            gestureObject.timerFlag = true
+            gestureObject.timer.invalidate()
+        }
+    }
+}
--- SampleFocus/ViewController.swift
+++ SampleFocus/ViewController.swift
@@ -8,93 +8,13 @@
 
 import UIKit
 
-struct CommonStructure {
-    // ジャスチャー
-    static var swipePanGesture = UIPanGestureRecognizer()
-}
+class ViewController: UIViewController {
 
-class ViewController: UIViewController,UIGestureRecognizerDelegate {
-
-    var gestureObject = GestureObject()
+    var g: GestureView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // ジャスチャー
-        CommonStructure.swipePanGesture = UIPanGestureRecognizer(target: self, action:#selector(panTapped))
-        CommonStructure.swipePanGesture.delegate = self
-
-        view.addGestureRecognizer( CommonStructure.swipePanGesture)
-        view.backgroundColor = .blue
-        
-        gestureObject.imageView?.image = UIImage(named: "Mac")
-        gestureObject.imageView?.contentMode = .scaleAspectFit
-        gestureObject.imageView?.frame = CGRect(x: 30, y: 30, width: view.frame.width - 60, height: view.frame.height - 30)
-        view.addSubview(gestureObject.imageView ?? UIImageView())
-        // レイヤーのマスキング
-        gestureObject.cALayerView?.tori(gestureObject, bool: true)
-        
-        view.layer.addSublayer(gestureObject.cALayerView?.hollowTargetLayer ?? CALayer())
-        view.addSubview(gestureObject.cALayerView ?? UIView())
-        
-        view.addSubview(gestureObject.lineDashView ?? UIView())
-        gestureObject.lineDashView?.isHidden = true
-    }
-
-    @objc func panTapped(sender:UIPanGestureRecognizer) {
-        let position: CGPoint = sender.location(in: view)
-        //画面をなぞる場合にフォーカスの設定
-        self.gestureObject.cALayerView?.effect(gestureObject, bool: false)
-        self.gestureObject.cALayerView?.tori(gestureObject, bool: false)
-        switch sender.state {
-        case .ended:
-            //指が離れた際の座標を取得
-            gestureObject.endPoint = gestureObject.lineDashView?.frame.origin ?? CGPoint()
-            gestureObject.endFrame = gestureObject.lineDashView?.frame ?? CGRect()
-            //フォーカス計算のメソッド呼び出し
-            gestureObject.timerFlag = false
-            timerSetting()
-            break
-        case .possible:
-            break
-        case .began:
-            gestureObject.lineDashView?.isHidden = false
-            gestureObject.timerFlag = true
-            gestureObject.timer.invalidate()
-            //タップした領域を取得
-            gestureObject.touchFlag = self.gestureObject.cropEdgeForPoint(point: gestureObject.framePoint)
-            break
-        case .changed:
-             //タップされた領域からMaskするViewのサイズ、座標計算
-            self.gestureObject.updatePoint(point: position,touchFlag: self.gestureObject.touchFlag)
-            break
-        case .cancelled:
-            break
-        case .failed:
-            break
-        @unknown default:
-            fatalError()
-        }
-    }
-
-    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
-        let position: CGPoint = touch.location(in: view)
-        gestureObject.framePoint = position
-        return true
-    }
-
-    func timerSetting() {
-        gestureObject.timer = Timer.scheduledTimer(timeInterval:3.0,
-                                     target: self,
-                                     selector: #selector(animetionSet),
-                                     userInfo: nil,
-                                     repeats: false)
-    }
-
-    @objc func animetionSet() {
-        if gestureObject.timerFlag == false {
-            gestureObject.matchGround()
-            gestureObject.timerFlag = true
-            gestureObject.timer.invalidate()
-        }
+        g = GestureView(view.frame ,imageSt: "Mac")
+        view.addSubview(g ?? UIView())
     }
 }
